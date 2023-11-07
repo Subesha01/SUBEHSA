77a7af00e3a1c427a8211b67bb62d1cc97ffece5
Subesha01 <727722euit188@skcet.ac.in>
2023-11-07 12:34:10 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-import {Message} from "./Message"
+import "./App.css";
+import NavBar from "./components/NavBar";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 function App() {
-  return <Message></Message>
-    
-    
-  
-}
+  return (
+    <div>
+    <NavBar/>
+    <Main/>
+    <Footer/>
+    </div>
+  );
+    }
    
 export default App;
