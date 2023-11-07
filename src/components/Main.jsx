a1fb78d3d0e61ce204b596e8fc5421dd2182a7fd
Subesha01 <727722euit188@skcet.ac.in>
2023-11-07 15:25:27 +0530

    commit

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,12 +4,14 @@ const Main = () => {
   return (
     <div>
       <label for="name">Email Id: &emsp;</label>
-      <input id="name" type="box" placeholder="Email ID"></input>
+      <input id="name" type="Email Id" placeholder="Email ID"></input>
       <br></br>
       <label for="na">Password: &emsp;</label>
-      <input id="na" type="box" placeholder="Password"></input>
+      <input id="na" type="Password" placeholder="Password"></input>
       <br></br>
-      <button>Submit</button>
+      <center>
+        <button>Submit</button>
+      </center>
     </div>
   );
 };
