2ea8c8fe16802de779559f23781d18507b5f8bce
Subesha01 <727722euit188@skcet.ac.in>
2023-11-08 11:03:07 +0530

    commit

---
+++ src/day3/DynamicRendering.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+const DynamicRendering = () => {
+  const arr = ["ReactJS", "AngularJS", "VueJS", "NextJS", "JavaScript"];
+  return (
+    <ul>
+      {arr.map((element) => (
+        <li>{element}</li>
+      ))}
+    </ul>
+  );
+};
+export default DynamicRendering;
+b;
